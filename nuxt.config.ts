03c7ff42558e5f6a9b9dd55eebf664c756cb4944
ivmendoz <ivmtrab183@gmail.com>
2026-09-24 00:04:31 -0400

    Config para deploy en versel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,19 +4,17 @@
 //   devtools: { enabled: true }
 // })
 
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
-  modules: [
-    '@pinia/nuxt' // Removimos '@nuxtjs/tailwindcss'
-  ],
+  css: ["~/assets/css/main.css"],
+  modules: ["@pinia/nuxt"],
+  nitro: {
+    preset: "vercel",
+  },
   vite: {
-    plugins: [
-      tailwindcss(),
-    ]
+    plugins: [tailwindcss()],
   },
 });
